bba205d1df2dca66a53bf52d3be08cc8cf40ce00
diabuz1 <diasbuzanov@gmail.com>
2026-05-12 22:00:00 +0600

    set up proj

---
+++ src/main/java/com/example/urlanalytics/UrlAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package com.example.urlanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlAnalyticsApplication.class, args);
+    }
+}
